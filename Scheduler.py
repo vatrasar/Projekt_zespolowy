cf0d0bf9c6cc1993e18d1dd187134b493ebee6f0
vatrasar <vatrasar@gmail.com>
2018-11-13 14:13:00 +0100

    add 'active' attribute to Sensor class

--- Scheduler.py
+++ Scheduler.py
@@ -18,15 +18,17 @@ class Scheduler:
 
 class Sensor:
 
-    def __init__(self,battery,sensing_range,localization):
+    def __init__(self,battery,sensing_range,localization,active):
         """
         :param battery:
         :param sensing_range:typ Point
         :param localization: tylko przy rozwiązaniu dystrybucyjnym(zachłannynm)
+	:param active: true gdy sensor aktywny,false gdy nieaktywny 
         """
         self.battery=battery
         self.sensing_range=sensing_range
         self.localization=localization
+	self.active=active
 
 
 class Point:
